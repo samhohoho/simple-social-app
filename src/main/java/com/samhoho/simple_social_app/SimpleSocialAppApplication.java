4f188e6ca797cfc3ae850e36435c80698a533f33
hosam <hosamchung998@gmail.com>
2026-01-18 23:12:04 +0800

    setup

---
+++ src/main/java/com/samhoho/simple_social_app/SimpleSocialAppApplication.java
@@ -0,0 +1,13 @@
+package com.samhoho.simple_social_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSocialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samhoho/simple_social_app/SimpleSocialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhoho.simple_social_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSocialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
